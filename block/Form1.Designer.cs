10d92eb3f55c121b9db443cea164813265053266
Basukov Andrei <dron221297@gmail.com>
2018-04-10 12:14:23 +0500

    release v1

---
+++ block/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace block
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(666, 459);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Изменить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(275, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Закоммитить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(275, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Отобразить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Поле \"Name\" id=112";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 584);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ block/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace block
+{
+    public partial class Form1 : Form
+    {
+      
+        DataSet dataSet = new DataSet();
+        DataTable dataTable = new DataTable();
+        NpgsqlTransaction tr ;
+        NpgsqlConnection con ;
+        NpgsqlCommand command ;
+        public Form1()
+        {
+        
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string name = textBox1.Text;
+            string manuf = "test";
+            string sql = "";
+            int nowRow = dataGridView1.CurrentRow.Index;
+            try
+            {
+                sql = "SELECT* FROM drug WHERE id = " + dataGridView1.Rows[nowRow].Cells[0].Value + " FOR UPDATE NOWAIT";
+                command = new NpgsqlCommand(sql, con);
+                command.ExecuteNonQuery();
+
+                sql = "UPDATE drug  SET name = '" + name + "', manufacturer = '" + manuf + "' WHERE id = " + dataGridView1.Rows[nowRow].Cells[0].Value  /*+ " and pg_try_advisory_xact_lock(tableoid::INTEGER,id)"*/;             
+                command = new NpgsqlCommand(sql, con);
+                command.Transaction = tr;
+                command.ExecuteNonQuery();
+
+              
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                //MessageBox.Show(tr.IsolationLevel.ToString());
+                tr.Commit();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                dataSet.Clear();
+                string constr = "Server=127.0.0.1; Port=5432; User Id=postgres; Password=22; Database=polyclinicver3;";
+                con = new NpgsqlConnection(constr);
+                con.Open();
+                tr = con.BeginTransaction(IsolationLevel.Serializable);
+
+                command = con.CreateCommand();
+                command.Transaction = tr;
+
+                string query = "SELECT * FROM drug WHERE id > 111";
+
+                command = new NpgsqlCommand(query, con);
+                NpgsqlDataAdapter adap = new NpgsqlDataAdapter(command);
+                adap.Fill(dataSet, "base");
+                dataGridView1.DataSource = dataSet.Tables[0];
+                textBox1.Text = Convert.ToString(dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[1].Value);
+
+                //tr.Commit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                //tr.Rollback();
+            }
+        }
+    }
+}
